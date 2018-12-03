ad3ba6c2320361ddb735155fa064310b54900983
Zach Lehman <zalehman182@stevenscollege.edu>
2018-12-03 08:50:59 -0500

    Commented out the code and tweaked code for understanding

--- main.js
+++ main.js
@@ -1,51 +1,49 @@
 //Function for the Win
-/*
-const winFunc = () => {
-    let row;
-    let column;
-    let pos1;
-    let pos2;
-    let pos3;
-    let pos4;
-    let maxMoves = rows * columns;
-};
-*/
 const check = (position1, position2, position3, position4) => {
     return ((position1 != 0) && (position1 == position2) && (position1 == position3) && (position1 == position4));
 }
 
 const checkForWinner = (win) => {
-    // Horizontally
+    // Vertically Winning
     for (row = 0; row <= 2; row++)
         for (column = 0; column <= 6; column++)
+        //Running to for loops one for the rows and the other for the columns.
             if (check(win[row][column], win[row + 1][column], win[row + 2][column], win[row + 3][column]))
+            //This checks if there is a win the columns
                 return win[row][column];
 
-    // Vertically
+    // Horizontally Winning
     for (row = 0; row <= 5; row++)
         for (column = 0; column <= 3; column++)
+        //Running to for loops one for the rows and the other for the columns.
             if (check(win[row][column], win[row][column + 1], win[row][column + 2], win[row][column + 3]))
+            //This checks if there is a win the rows
                 return win[row][column];
 
-    // Diagonal
+    // Diagonal one way (Bottom to Top)
     for (row = 0; row <= 2; row++)
         for (column = 0; column <= 3; column++)
+        //Running to for loops one for the rows and the other for the columns.
             if (check(win[row][column], win[row + 1][column + 1], win[row + 2][column + 2], win[row + 3][column + 3]))
+            //It checks the Diagonal from bottom to top.
                 return win[row][column];
 
-    // Diagonal
+    // Diagonal the other way (Top to Bottom)
     for (row = 3; row <= 5; row++)
         for (column = 0; column <= 3; column++)
+        //Running to for loops one for the rows and the other for the columns.
             if (check(win[row][column], win[row - 1][column + 1], win[row - 2][column + 2], win[row - 3][column + 3]))
+            //It checks the Diagonal and it is from top to bottom.
                 return win[row][column];
 
-    return 0;
-}
-
+    return 'No Winner Yet';
+    //If there is no winner yet it will just be 0 until there is a winner
+};
+//This just a test case to see if it actually works
 x =[ [0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 2, 1, 0, 0],
-     [0, 0, 0, 2, 1, 0, 0],
-     [0, 1, 0, 2, 2, 2, 0],
-     [0, 1, 2, 2, 1, 2, 0] ];
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0] ];
 console.log(checkForWinner(x));
